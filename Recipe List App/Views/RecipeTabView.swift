31816586a7ee999ddc038e9736864e2ea69f450d
Denis O'Brien <denisobrienjhb@gmail.com>
2022-04-26 05:57:25 -0400

    - Create RecipeTabView - Added Tabs

---
+++ Recipe List App/Views/RecipeTabView.swift
@@ -0,0 +1,38 @@
+//
+//  RecipeTabView.swift
+//  Recipe List App
+//
+//  Created by Denis O'Brien on 4/26/22.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    @State var tabIndex = 0
+    var body: some View {
+        TabView(selection: $tabIndex) {
+            Text("Featured View")
+                .tabItem {
+                    VStack{
+                        Image(systemName: "star.fill")
+                        Text("Featured")
+                    }
+                }
+                .tag(1)
+            RecipeListView()
+                .tabItem {
+                    VStack{
+                        Image(systemName: "list.bullet")
+                        Text("List")
+                    }
+                }
+                .tag(2)
+        }
+    }
+}
+
+struct RecipeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeTabView()
+    }
+}
